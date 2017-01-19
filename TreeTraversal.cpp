f74372df8ed8315c83024db50c7238d47259e197
shagun <shagun@localhost.localdomain>
2017-01-20 01:43:49 +0530

    tree traversals

---
+++ TreeTraversal.cpp
@@ -0,0 +1,125 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node {
+
+	int val;
+	Node *left;
+	Node *right;
+
+	Node(int val, Node *left, Node *right) {
+		this->val = val;
+		this->left = left;
+		this->right = right;
+	}
+};
+
+void inorderTraversal(Node *root) {
+	if(NULL == root) return;
+	
+	inorderTraversal(root->left);
+	cout<<root->val<<" ";
+	inorderTraversal(root->right);
+}
+
+void preOrderTraversal(Node *root) {
+
+	if(NULL == root) return;
+	
+	preOrderTraversal(root->left);
+	preOrderTraversal(root->right);
+	cout<<root->val<<" ";
+}
+
+void postOrderTraversal(Node *root) {
+
+	if(NULL == root) return ;
+	
+	cout<<root->val<<" ";
+	postOrderTraversal(root->left);
+	postOrderTraversal(root->right);
+}
+
+void printInRange(Node *root, int k1, int k2) {
+	if(NULL == root) return ;
+
+	if(root->val > k1) {
+		printInRange(root->left, k1, k2);
+	}
+	if(root->val >= k1 && root->val <=k2) {
+		cout<<root->val<<" ";
+	}
+	if(root->val < k2) {
+		printInRange(root->right, k1, k2);
+	}
+}
+
+void printChild(Node *root) {
+
+	if(NULL == root) return;
+	cout<<"(";
+	if(NULL != root->left) cout<<root->left->val;
+	else cout<<"NULL";
+
+	cout<<",";
+	
+	if(NULL != root->right) cout<<root->right->val;
+	else cout<<"NULL";
+
+	cout<<")\n";
+}
+
+void printTree(Node *root) {
+
+	if(NULL == root) return ;
+	
+	cout<<root->val<<"->";
+	printChild(root);
+	printTree(root->left);
+	printTree(root->right);
+}
+
+
+
+int main() {
+
+	Node *one = new Node(1, NULL, NULL);
+	Node *four = new Node(4, NULL, NULL);
+	Node *three = new Node(3, one, four);
+	
+	Node *seven = new Node(7, NULL, NULL);
+	Node *nine = new Node(9, NULL, NULL);
+	Node *eight = new Node(8, seven, nine);
+
+	Node *five = new Node(5, three, eight);
+
+	Node *fifteen = new Node(15, NULL, NULL);
+	Node *twentyFive = new Node(25, NULL, NULL);
+	Node *twenty = new Node(20, fifteen, twentyFive);
+	
+	Node *ten = new Node(10, five, twenty);
+	
+	Node *root = ten;
+
+	cout<<"Tree (Node->(left, right) :\n";
+	printTree(root);
+	cout<<endl;
+
+	cout<<"Inorder Traversal: ";
+	inorderTraversal(root);
+	cout<<endl;
+
+	cout<<"Preorder Traversal: ";
+	preOrderTraversal(root);
+	cout<<endl;
+	
+	cout<<"Postorder Traversal: ";
+	postOrderTraversal(root);
+	cout<<endl;
+
+	cout<<"Print in range: ["<<5<<","<<15<<"] :";
+	printInRange(root,5, 15);
+	cout<<endl;
+	return 0;
+}
